18febef1badacdb30ad3df0d86bcf56b814533fb
Hoangtrung <trung.vp91@gmai.com>
2023-07-05 10:50:38 +0700

    done home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Footer, NavBar } from '@/components'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='relative'>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
